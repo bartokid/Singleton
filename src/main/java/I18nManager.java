0e51d15c44d57bad1efea4ac4d1d08e1fe911985
bartokid <96bartolipau@gmail.com>
2019-03-21 16:59:55 +0100

    falta: getText i constructor i18nManager

--- src/main/java/I18nManager.java
+++ src/main/java/I18nManager.java
@@ -5,8 +5,19 @@ import org.apache.log4j.*;
 public class I18nManager {
     static Logger logger = Logger.getLogger(I18nManager.class);
     private HashMap<String, ResourceBundle> data;
-    public static void  getInstance(){}
+    private static I18nManager single_instance = null;
+    public static I18nManager getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new I18nManager();
 
+        return single_instance;
+    }
+
+
+    public String getText(String property, String key){
+        return "hola";
+    }
 
     public String getString(String lang, String key) {
         ResourceBundle rs = data.get(lang);
