0af80e83f37f791a9516536e8c16efc0bda2792f
bartokid <96bartolipau@gmail.com>
2019-03-21 16:47:43 +0100

    maven test

--- src/main/java/I18nManager.java
+++ src/main/java/I18nManager.java
@@ -1,2 +1,20 @@
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
 public class I18nManager {
+
+    private HashMap<String, ResourceBundle> data;
+    public static void  getInstance(){}
+
+
+    public String getString(String lang, String key) {
+        ResourceBundle rs = data.get(lang);
+
+        if (rs == null) {
+            //logger.info("classloader")
+            rs = ResourceBundle.getBundle(lang);
+            data.put(lang, rs);
+        }
+        return rs.getString(key);
+    }
 }
--- src/test/java/I18nManagerTest.java
+++ src/test/java/I18nManagerTest.java
@@ -1,2 +1,20 @@
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+
 public class I18nManagerTest {
+
+    @Test
+    public void Test(){
+        String text1 = I18nManager.getInstance().getText("en", "l1");
+        Assert.assertEquals("Hello", text1);
+
+        String text2 = I18nManager.getInstance().getText("es", "l1");
+        Assert.assertEquals("Hola", text2);
+
+        String text3 = I18nManager.getInstance().getText("es", "l2");
+        Assert.assertEquals("Adios", text2);
+    }
 }
